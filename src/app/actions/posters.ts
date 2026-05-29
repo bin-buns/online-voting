238a1f62347b17efe119cbd7e103228af44515fd
bin-buns <vincesenores@gmail.com>
2026-05-29 13:46:27 +0800

    add missing posters.ts action file

---
+++ src/app/actions/posters.ts
@@ -0,0 +1,11 @@
+export type PosterWithCandidate = {
+  id: string;
+  title: string;
+  content: string;
+  image_url: string | null;
+  created_at: string;
+  candidate_id: string;
+  author: string;
+  position: string;
+  photo_url: string | null;
+};
